489fc09bb3d701a07fb6105fa1080a68c8a2dbd7
deniszayakin13 <deniszayakin13@gmail.com>
2026-04-14 13:55:24 +0300

    Update n2.py

--- n2.py
+++ n2.py
@@ -1,10 +1,1 @@
-def filtered_books(books, keyword):
-    matches = filter(lambda b: keyword.lower() in b[1].lower(), books)
-    return list(map(
-        lambda b: [b[0], f"{b[1]}, {b[2]}", b[3], b[4]],
-        matches
-    ))
 
-books = get_books("books.csv")
-result = filtered_books(books, "python")
-print(result)
