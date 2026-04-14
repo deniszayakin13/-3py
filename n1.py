668a6cfb9363763c889538af75c8f983e277bb83
deniszayakin13 <deniszayakin13@gmail.com>
2026-04-14 13:53:28 +0300

    Update n1.py

--- n1.py
+++ n1.py
@@ -13,3 +13,21 @@ def get_books(file_name):
 
 get_books("books.csv")
 input("Нажмите Enter, чтобы выйти...")
+def filtered_books(books, keyword):
+    matches = filter(lambda b: keyword.lower() in b[1].lower(), books)
+    return list(map(
+        lambda b: [b[0], f"{b[1]}, {b[2]}", b[3], b[4]],
+        matches
+    ))
+
+books = get_books("books.csv")
+result = filtered_books(books, "python")
+print(result)
+def books_total(books):
+    return tuple(list(map(
+        lambda b: (b[0], b[3] * b[4]),
+        books
+    )))
+books = get_books("books.csv")  
+totals = books_total(books)
+print(totals)
