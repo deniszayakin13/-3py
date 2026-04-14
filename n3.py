d92eef706b765dc4619c0a7d61b96e2660ad488a
deniszayakin13 <deniszayakin13@gmail.com>
2026-04-14 13:55:01 +0300

    Update n3.py

--- n3.py
+++ n3.py
@@ -1,8 +1,1 @@
-def books_total(books):
-    return tuple(list(map(
-        lambda b: (b[0], b[3] * b[4]),
-        books
-    )))
-books = get_books("books.csv")  
-totals = books_total(books)
-print(totals)
+
